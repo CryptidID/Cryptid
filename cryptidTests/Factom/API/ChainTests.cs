97a54629899d86e3753ea4291b8735a79952b92d
Steven Masley <steven@slackerbots.com>
2015-11-09 22:47:43 -0500

    My intial commit

---
+++ cryptidTests/Factom/API/ChainTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cryptid.Factom.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptid.Factom.API.Tests {
+    [TestClass()]
+    public class ChainTests {
+        [TestMethod()]
+        public void getChainTest() {
+            Chain api = new Chain();
+            string key = api.GetChainHead("df3ade9eec4b08d5379cc64270c30ea7315d8a8a1a69efe2b98a60ecdd69e604");
+            Console.WriteLine("KeyMR: " + key);
+            string entryBlockStruct = api.GetEntryBlockByKeyMR(key);
+            Console.WriteLine("EntryBlock: " + entryBlockStruct);
+
+        }
+    }
+}
