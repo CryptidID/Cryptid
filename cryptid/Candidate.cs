ccb1e6f0f29aa7dbd149392f898a6860164e9372
Dakota Baber <dakota@baber.io>
2015-11-09 20:11:01 -0500

    Initial

---
+++ cryptid/Candidate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cryptid
+{
+    public class Candidate {
+        public String Dcs { get; set; }
+        public String Dac { get; set; }
+        public String Dad { get; set; }
+        public String Dbd { get; set; }
+        public String Dbb { get; set; }
+        public String Dbc { get; set; }
+        public String Day { get; set; }
+        public String Dau { get; set; }
+        public String Dag { get; set; }
+        public String Dai { get; set; }
+        public String Daj { get; set; }
+        public String Dak { get; set; }
+        public String Dcg { get; set; }
+
+        public byte[] image { get; set; }
+        public byte[] fingerprint { get; set; }
+    }
+}
