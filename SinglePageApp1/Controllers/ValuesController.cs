607e10449f8e3c47f4c3ced0da54b4c012bc159a
Dakota Baber <dakota@baber.io>
2015-11-21 03:14:15 -0500

    Start rest api

---
+++ SinglePageApp1/Controllers/ValuesController.cs
@@ -0,0 +1,9 @@
+﻿namespace SinglePageApp1.Controllers {
+    using System.Web.Http;
+
+    public class ValuesController : ApiController {
+        public IHttpActionResult GetValues() {
+            return Ok(new[] { "a", "b", "c" });
+        }
+    }
+}
