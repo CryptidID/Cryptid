9c37807a63e03699f26e51b6c71fdd15b9487887
Dakota Baber <dakota@baber.io>
2015-11-21 13:11:05 -0500

    Use SelfHosteHttpServer instead of web api stuff

--- CryptidRest/Properties/AssemblyInfo.cs
+++ CryptidRest/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("38eae5b7-8795-48e5-8ece-7488f1df4193")]
+[assembly: Guid("ccdfc811-8d7d-4807-adf8-d9470a596379")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
--- SinglePageApp1/Controllers/ValuesController.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SinglePageApp1.Controllers {
-    using System.Web.Http;
-
-    public class ValuesController : ApiController {
-        public IHttpActionResult GetValues() {
-            return Ok(new[] { "a", "b", "c" });
-        }
-    }
-}
