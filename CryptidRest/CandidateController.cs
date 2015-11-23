f00e7484405231ed534159336fc2cb0639d06dd6
Dakota Baber <dakota@baber.io>
2015-11-22 22:13:17 -0500

    Add load from block chain. Fix candidate controller.

--- CryptidRest/CandidateController.cs
+++ CryptidRest/CandidateController.cs
@@ -1,89 +1,4 @@
-<<<<<<< HEAD
-﻿#region
-
-using System;
-using System.Drawing;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-using Cryptid;
-using Cryptid.Utils;
-using SourceAFIS.Simple;
-
-#endregion
-
-namespace CryptidRest.Controllers {
-    [RoutePrefix("api/candidate")]
-    public class CandidateController : ApiController {
-        private const string PublicKeyPath = "public.xml";
-
-        [Route("FullVerifyFromChain/{chainIdBase64}/{password}/{fpImageBase64}")]
-        [HttpPost]
-        public float FullVerifyFromChain(string chainIdBase64, string password, string fpImageBase64) {
-            try {
-                using (var ms = new MemoryStream(Convert.FromBase64String(fpImageBase64))) {
-                    var b = new Bitmap(ms);
-                    var f = new Fingerprint { AsBitmap = b };
-                    return CandidateDelegate.FullVerifyFromChain(Convert.FromBase64String(chainIdBase64), password, f,
-                        Keys.PublicKey(PublicKeyPath));
-                }
-            } catch (Exception e) {
-                var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError) {
-                    Content = new StringContent(e.Message),
-                    ReasonPhrase = "Internal Error Occured"
-                };
-                throw new HttpResponseException(resp);
-            }
-        }
-
-        [Route("GetCandidateFromChain/{chainIdBase64}/{password}")]
-        [HttpGet]
-        public Candidate GetCandidateFromChain(string chainIdBase64, string password) {
-            try {
-                var packed = CandidateDelegate.GetPackedCandidate(Convert.FromBase64String(chainIdBase64));
-                return CandidateDelegate.Unpack(packed, password, Keys.PublicKey(PublicKeyPath));
-            } catch (Exception e) {
-                var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError) {
-                    Content = new StringContent(e.Message),
-                    ReasonPhrase = "Internal Error Occured"
-                };
-                throw new HttpResponseException(resp);
-            }
-        }
-
-        [Route("UnpackCandidate/{packedBase64}/{password}")]
-        [HttpPost]
-        public Candidate UnpackCandidate(string packedBase64, string password) {
-            try {
-                return CandidateDelegate.Unpack(Convert.FromBase64String(packedBase64), password,
-                    Keys.PublicKey(PublicKeyPath));
-            } catch (Exception e) {
-                var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError) {
-                    Content = new StringContent(e.Message),
-                    ReasonPhrase = "Internal Error Occured"
-                };
-                throw new HttpResponseException(resp);
-            }
-        }
-
-        [Route("UnpackCandidate/{packedBase64}")]
-        [HttpPost]
-        public bool VerifySignature(string packedBase64) {
-            try {
-                return CandidateDelegate.VerifySignature(Convert.FromBase64String(packedBase64),
-                    Keys.PublicKey(PublicKeyPath));
-            } catch (Exception e) {
-                var resp = new HttpResponseMessage(HttpStatusCode.InternalServerError) {
-                    Content = new StringContent(e.Message),
-                    ReasonPhrase = "Internal Error Occured"
-                };
-                throw new HttpResponseException(resp);
-            }
-        }
-    }
-=======
-﻿#region
+#region
 
 using System;
 using System.Drawing;
@@ -102,11 +17,13 @@ namespace CryptidRest.Controllers {
     [RoutePrefix("api/candidate")]
     public class CandidateController : ApiController {
         private class ApiError {
-            public ApiError(string msg) {
+            public ApiError(string name, string msg) {
+                ErrorName = name;
                 Error = msg;
             }
 
             public string Error { get; set; }
+            public string ErrorName { get; set; }
         }
 
         private static readonly string PublicKeyPath = "public.xml";
@@ -123,7 +40,7 @@ namespace CryptidRest.Controllers {
                 }
             } catch (Exception e) {
                 var resp = new HttpResponseMessage(HttpStatusCode.OK) {
-                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.Message))),
+                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.GetType().Name, e.Message))),
                     ReasonPhrase = "Internal Error Occured"
                 };
                 throw new HttpResponseException(resp);
@@ -138,7 +55,7 @@ namespace CryptidRest.Controllers {
                 return CandidateDelegate.Unpack(packed, password, Keys.PublicKey(PublicKeyPath));
             } catch (Exception e) {
                 var resp = new HttpResponseMessage(HttpStatusCode.OK) {
-                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.Message))),
+                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.GetType().Name, e.Message))),
                     ReasonPhrase = "Internal Error Occured"
                 };
                 throw new HttpResponseException(resp);
@@ -153,7 +70,7 @@ namespace CryptidRest.Controllers {
                     Keys.PublicKey(PublicKeyPath));
             } catch (Exception e) {
                 var resp = new HttpResponseMessage(HttpStatusCode.OK) {
-                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.Message))),
+                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.GetType().Name, e.Message))),
                     ReasonPhrase = "Internal Error Occured"
                 };
                 throw new HttpResponseException(resp);
@@ -168,12 +85,11 @@ namespace CryptidRest.Controllers {
                     Keys.PublicKey(PublicKeyPath));
             } catch (Exception e) {
                 var resp = new HttpResponseMessage(HttpStatusCode.OK) {
-                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.Message))),
+                    Content = new StringContent(JsonConvert.SerializeObject(new ApiError(e.GetType().Name, e.Message))),
                     ReasonPhrase = "Internal Error Occured"
                 };
                 throw new HttpResponseException(resp);
             }
         }
     }
->>>>>>> 0df6c7e647b2182ab7b585518937ca2820213597
 }
